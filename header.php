f67f1d64fcb9da906fe5e599df6ff8d9774adff0
web-commander <web.commander.gbdr@gmail.com>
2019-02-13 23:35:27 +0900

    xmlフォーマッタ用のライブラリを追加

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js" type="text/javascript"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/static/js/bootstrap.min.js"  type="text/javascript"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/static/js/custom.js"  type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/static/js/vkbeautify.js"  type="text/javascript"></script>
     <?php wp_head(); 
     ?>
   </head>
