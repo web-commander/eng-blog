adac41fbf900344464dfed8abe31906d71894490
web-commander <web.commander.gbdr@gmail.com>
2019-02-16 18:54:06 +0900

    カテゴリページを修正

--- category.php
+++ category.php
@@ -2,30 +2,27 @@
 
 <div class="col-md-8 blog-main">
 
-  <h1>カテゴリページ</h1>
+  <h1>
+  <?php
+    // カテゴリ名を取得
+    $category = get_the_category();
+    echo $category[0]->cat_name;
+  ?>
+  </h1>
   <section class="archive">
     <ul>
     <?php if (have_posts()): while (have_posts()) : the_post(); ?>
       <li>
         <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d'); ?></time>
-        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
       </li>
     <?php endwhile;endif; ?>
     </ul>
     <nav class="pagination-area">
-    <?php 
-    the_posts_pagination( array (
-      'prev_text' => 'PREV',
-      'next_text' => 'NEXT',
-      'mid_size'  => 2
-    ) );
+    <?php //the_posts_pagination(); 
     ?>
     </nav>
   </section>
-  <section class="sidebar">
-    <?php get_sidebar(); ?>
-  </section>
-
 </div>
 
 <?php get_sidebar(); ?>
