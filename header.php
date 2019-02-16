6ee128cd28effc0c625d51bc6170592faef01baf
web-commander <web.commander.gbdr@gmail.com>
2019-02-16 18:09:45 +0900

    パンくずリストを表示処理追加。titleタグ内の値をブログタイトルから取得する要修正

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta name="description" content="ページの内容を表す文章">
-    <title>とあるEngineerBlog</title>
+    <title><?php bloginfo( 'name' ); ?></title>
     <!-- favicon -->
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
     <!-- CSS -->
--- single.php
+++ single.php
@@ -1,8 +1,25 @@
 <?php get_header(); ?>
 
 <div class="col-md-8 blog-main">
-  <?php the_title('<h1 class="mt-3">','</h1>'); ?>
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <div class="breadcrumbs">
+  <?php
+    $categories = get_the_category();
+    $separator = ' ＞ ';
+    $output = '<a href="' .home_url() .'">' .'HOME' .'</a>';
+    if ( $categories ) {
+	    foreach( $categories as $category ) {
+    		$output .=  $separator .'<a href="' . get_category_link( $category->term_id ) . '" title="' 
+	    		. esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) 
+	    		. '">' . $category->cat_name . '</a>' ;
+    	}
+    $output .=  $separator;
+    //echo trim( $output, ' ');
+    echo $output;
+    }
+  ?> <?php the_title(); ?>
+  </div>
+  <?php the_title('<h1 class="mt-3">','</h1>'); ?>
   <?php the_content(); ?>
   <?php endwhile; endif; ?>
 </div>
