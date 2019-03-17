aff9e783e867e947d2f293b7d65db5807b08f62e
web-commander <web.commander.gbdr@gmail.com>
2019-03-17 18:26:35 +0900

    page.phpのバグ修正

--- page.php
+++ page.php
@@ -3,6 +3,7 @@
 <div class="container">
   <main role="main" class="container">
    <div class="row">
+     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
      <div class="breadcrumbs py-2 pl-5">
       <?php
       // パンくずリスト取得
@@ -17,8 +18,9 @@
         echo $output;
         ?> <?php the_title(); ?>
       </div>
-
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php the_title('<h1 class="mt-3">','</h1>'); ?>
+      <?php the_content(); ?>
+     <?php endwhile; endif; ?>
 
     </div> <!-- row -->
   </main>
