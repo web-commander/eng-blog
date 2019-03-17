63431dd91f30adfccce2846dfb26a8284eed263f
web-commander <web.commander.gbdr@gmail.com>
2019-03-17 18:19:18 +0900

    体裁修正

--- page.php
+++ page.php
@@ -3,29 +3,22 @@
 <div class="container">
   <main role="main" class="container">
    <div class="row">
+     <div class="breadcrumbs py-2 pl-5">
+      <?php
+      // パンくずリスト取得
+      $separator = ' ＞ ';
+      $output = '<a href="' .home_url() .'">' .'HOME' .'</a>';
+      foreach ( array_reverse(get_post_ancestors($post->ID)) as $parid ) {
+        $output .=    $separator .'<a href="' . get_page_link( $parid ) . '" title="' 
+          . esc_attr( sprintf( __( "View all posts in %s" ), get_page($parid)->post_title ) ) 
+          . '">' . get_page($parid)->post_title . '</a>' ;
+        }
+        $output .= $separator;
+        echo $output;
+        ?> <?php the_title(); ?>
+      </div>
 
-<div class="col-md-8 blog-main">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <div class="breadcrumbs">
-  <?php
-    // パンくずリスト取得
-    $separator = ' ＞ ';
-    $output = '<a href="' .home_url() .'">' .'HOME' .'</a>';
-    foreach ( array_reverse(get_post_ancestors($post->ID)) as $parid ) {
-    	$output .=    $separator .'<a href="' . get_page_link( $parid ) . '" title="' 
-    	. esc_attr( sprintf( __( "View all posts in %s" ), get_page($parid)->post_title ) ) 
-	    . '">' . get_page($parid)->post_title . '</a>' ;
-    }
-    $output .= $separator;
-    echo $output;
-  ?> <?php the_title(); ?>
-  </div>
-  <?php the_title('<h1 class="mt-3">','</h1>'); ?>
-  <?php the_content(); ?>
-  <?php endwhile; endif; ?>
-</div>
-
-<?php get_sidebar(); ?>
 
     </div> <!-- row -->
   </main>
